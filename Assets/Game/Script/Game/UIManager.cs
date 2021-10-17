9d2f34c91516d0e749a99d7a7835dabb69fae4be
Norah C. IV <Duynv.nde16013@vtc.edu.vn>
2021-10-17 16:33:10 +0700

    update ui

--- Assets/Game/Script/Game/UIManager.cs
+++ Assets/Game/Script/Game/UIManager.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game
 {
     public class UIManager : MonoSingleton<UIManager>
     {
-        public GameObject m_TapUI;
+        public GameObject m_HomeUI;
         public GameObject m_VictoryUI;
         public GameObject m_LoseUI;
+        public GameObject m_GameUI;
+        public GameObject m_LevelUI;
+
+        [Header("Camera")]
+        public GameObject m_GameCamera;
+        public GameObject m_ShopCamera;
+
+        [Header("Particles")]
+        public GameObject m_VictoryParticles;
+
+        [Header("Text")]
+        public Text m_LevelText;
 
         public void ButtonNext()
         {
@@ -34,28 +47,56 @@ namespace Game
         {
             base.Awake();
 
-            m_TapUI.SetActive(true);
+            m_LevelText.text = string.Format("Level {0}", RuntimeStorageData.PLAYER.level);
+
+            m_HomeUI.SetActive(true);
+            m_LevelUI.SetActive(true);
+
+            m_GameCamera.SetActive(true);
+
+
             m_VictoryUI.SetActive(false);
             m_LoseUI.SetActive(false);
+            m_GameUI.SetActive(false);
+
+            m_ShopCamera.SetActive(false);
         }
 
         public void TapToPlay()
         {
-            m_TapUI.SetActive(false);
+            m_HomeUI.SetActive(false);
             CameraController.Instance.MoveToDraw(() =>
             {
                 StaticVariable.GameState = GameState.DRAW;
             });
         }
 
+        public void ShowGameUI()
+        {
+            m_GameUI.SetActive(true);
+            m_LevelUI.SetActive(true);
+        }
+
         public void ShowVictoryUI()
         {
             m_VictoryUI.SetActive(true);
+            m_LevelUI.SetActive(false);
+            m_VictoryParticles.SetActive(true);
         }    
 
         public void ShowLoseUI()
         {
             m_LoseUI.SetActive(true);
+            m_LevelUI.SetActive(false);
         }    
+
+        public void OpenShop()
+        {
+            m_GameCamera.SetActive(false);
+            m_ShopCamera.SetActive(true);
+
+            m_LevelUI.SetActive(false);
+            m_HomeUI.SetActive(false);
+        }
     }
 }
