53b3b467d0e613018b4640c99ad72e8628a7ca7e
Norah C. IV <Duynv.nde16013@vtc.edu.vn>
2021-10-19 00:52:44 +0700

    update shop character

---
+++ Assets/Game/Script/Game/ShopCharacter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class ShopCharacter : MonoBehaviour
+    {
+        public Transform m_LHand;
+        public Transform m_RHand;
+        public Transform m_Head;
+        public Transform m_Back;
+    }
+}
